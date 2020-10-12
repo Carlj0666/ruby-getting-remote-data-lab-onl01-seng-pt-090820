d6ece1abc60e190ac1348aa84bcb60dfd4e4c8f9
Carl J <carlj1881@yahoo.com>
2020-10-12 21:28:39 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,7 +17,7 @@ class GetRequester
   
   def get_response_body
     uri = URI.parse(@url)
-    response = Net::HTTP.get_response_body(uri)
+    response = Net::HTTP(uri)
     response.body
   end
   
